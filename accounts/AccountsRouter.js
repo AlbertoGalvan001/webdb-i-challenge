d58ee32a3d1a4fd07ed2c3381c6f2dc2b4d47693
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2019-12-09 16:39:58 -0600

    CRUD operations completed

--- accounts/AccountsRouter.js
+++ accounts/AccountsRouter.js
@@ -15,3 +15,58 @@ router.get('/', (req, res) => {
             res.status(500).json({ error: 'Error getting accounts from database.' });
         });
 });
+
+router.get('/:id', (req, res) => {
+    knex
+        .select('*')
+        .from('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(post => {
+            res.status(200).json({ post })
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "Error getting the post." });
+        });
+
+});
+
+router.post('/', (req, res) => {
+
+    knex
+        .insert(req.body, "id")
+        .into('accounts')
+        .then(info => res.status(200).json(info))
+        .catch(error => res.status(500).json({ error: "Error posting info." }))
+});
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    knex('accounts')
+        .where({ id: id })
+        .update(changes)
+        .then(count => {
+            res.status(200).json(count)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "Error updating info." })
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    knex('accounts')
+        .where({ id: id })
+        .del()
+        .then(info => {
+            res.status(200).json(info)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "Error deleting info." })
+        });
+});
+
+module.exports = router;
